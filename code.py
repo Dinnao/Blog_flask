6890ada778ca135c8efa379ba3ea924bdbf8dbb7
Dima Topchiy <dima17894@gmail.com>
2021-08-13 22:54:18 +0300

    some

---
+++ code.py
@@ -0,0 +1,74 @@
+#cursor.execute('''CREATE TABLE IF NOT EXISTS posts (id integer primary key AUTOINCREMENT, title text, description text, date text)''')
+import sqlite3
+from flask import Flask, request, render_template, redirect
+from datetime import date
+
+app = Flask(__name__)
+
+
+# main page which shows all posts,
+# and redirects to the main page
+@app.route('/main')
+def main():
+    connection = sqlite3.connect('blog.sqlite3')
+    cursor = connection.cursor()
+    cursor.execute('''SELECT * FROM posts''')
+    posts = cursor.fetchall()
+    return render_template('index.html', posts=posts)
+
+
+# page which writes title and description of posts,
+# and redirects to the main page
+@app.route('/title/description')
+def title_description():
+    post_date = date.today()
+    connection = sqlite3.connect('blog.sqlite3')
+    cursor = connection.cursor()
+    cursor.execute('''CREATE TABLE IF NOT EXISTS posts (id integer primary key AUTOINCREMENT, title text, description text, date text)''')
+    title = request.args.get('title')
+    description = request.args.get('description')
+    if not title:
+        return 'Please insert title!'
+    if not description:
+        return 'Please insert description!'
+    cursor.execute('''INSERT INTO posts (title, description, date) VALUES (?, ?, ?)''', (title, description, post_date))
+    connection.commit()
+    connection.close()
+    return redirect('/main')
+
+
+# page which updates title&description of the posts,
+# and redirects to the main page
+@app.route('/update')
+def update():
+    connection = sqlite3.connect('blog.sqlite3')
+    cursor = connection.cursor()
+    title_to_change = request.args.get('title')
+    descr_to_change = request.args.get('description')
+    post_id = request.args.get('id')
+    if not title_to_change:
+        return 'Please enter the title you want to change!'
+    if not descr_to_change:
+        return 'Please enter the description you want to change!'
+    if not post_id:
+        return 'Please enter post id!'
+    cursor.execute('''UPDATE posts SET (title, description) = (?, ?) WHERE id = ? ''', (title_to_change, descr_to_change, post_id))
+    connection.commit()
+    connection.close()
+    return redirect('/main')
+
+
+# page which deletes posts by id,
+# and redirects to the main page
+@app.route('/delete')
+def delete():
+    connection = sqlite3.connect('blog.sqlite3')
+    cursor = connection.cursor()
+    id_to_delete = request.args.get('id')
+    if not id_to_delete:
+        return 'Please enter post id!'
+    cursor.execute('''DELETE FROM posts WHERE id = ? ''', id_to_delete)
+    connection.commit()
+    connection.close()
+    return redirect('/main')
+
